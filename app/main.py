f7e8df196c44b5c720b66547f136ab25316c921e
Raymond NGUYEN THANH <raymond.nguyenthanh@gmail.com>
2025-01-02 16:51:42 +0000

    Debug 2

--- app/main.py
+++ app/main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline !!"}
+    return {"Hello": "We Love Datascientest, and we did it. We build a CI/CD Pipeline !!>>>111"}
 
 
